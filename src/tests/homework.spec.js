2c349397a628c66af89eebea56c10f90741ece18
Ivana Jindrová <ivajindrova@gmail.com>
2024-10-14 17:50:36 +0200

    Initial commit

--- src/tests/homework.spec.js
+++ src/tests/homework.spec.js
@@ -3,7 +3,7 @@ import { test } from "@playwright/test";
 test("should open login page", async ({ page }) => {
     //přechod na stránku přihlášení + výpis do konzole
     await page.goto("/prihlaseni");
-    //console.log("To koukáš co? :D");
+    console.log("To koukáš co? :D");
     await page.setViewportSize({ width: 800, height: 600 });
     await page.screenshot({ path: "prvni_vystrizek_800_600.png" });
 
--- src/tests/lekce2.spec.js
+++ src/tests/lekce2.spec.js
@@ -4,11 +4,9 @@ test("should open login page", async ({ page }) => {
     //přechod na stránku přihlášení + výpis do konzole
     await page.goto("/prihlaseni");
     //console.log("To koukáš co? :D");
+    //dle tagu
+    
     await page.setViewportSize({ width: 800, height: 600 });
     await page.screenshot({ path: "prvni_vystrizek_800_600.png" });
 
-    await page.setViewportSize({ width: 920, height: 1080 });
-    await page.screenshot({ path: "druhy_vystrizek_920_1080.png" });
-
-    await page.pause(5000);
 });
